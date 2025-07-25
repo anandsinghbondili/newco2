e8e7dabc0bb25b7b93136db4d610822f6aa8e742
Anand Singh Bondili <anandsinghbondili@gmail.com>
2025-07-25 15:52:16 +0530

    commented AuthGuard for login

--- frontend/src/app/(appsettings)/layout.tsx
+++ frontend/src/app/(appsettings)/layout.tsx
@@ -2,7 +2,7 @@ import AppSidebar from "@/components/common/AppSidebar";
 import Navbar from "@/components/common/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import AuthGuard from "@/components/auth/AuthGuard";
+// import AuthGuard from "@/components/auth/AuthGuard";
 import { cookies } from "next/headers";
 
 export default async function RootLayout({
@@ -21,15 +21,15 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
         >
-            <AuthGuard>
-                <SidebarProvider defaultOpen={defaultOpen}>
-                    <AppSidebar />
-                    <main className="w-full">
-                        <Navbar />
-                        <div className="px-4">{children}</div>
-                    </main>
-                </SidebarProvider>
-            </AuthGuard>
+            {/* <AuthGuard> */}
+            <SidebarProvider defaultOpen={defaultOpen}>
+                <AppSidebar />
+                <main className="w-full">
+                    <Navbar />
+                    <div className="px-4">{children}</div>
+                </main>
+            </SidebarProvider>
+            {/* </AuthGuard> */}
         </ThemeProvider>
     );
 }
